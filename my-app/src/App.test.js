37deb1a144d228bc091328219f8fe949c91907af
Jay <jayliu@zymber.tech>
2023-04-18 16:31:39 +0800

    <> 文字 顏色

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // 不分大小寫
+  const linkElement = screen.getByText(/goto react/i);
   expect(linkElement).toBeInTheDocument();
 });
